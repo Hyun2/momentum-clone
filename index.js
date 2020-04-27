f950bf64017b3d94263e88e283c7a3fab4d95914
Hyun2 <hks0284@gmail.com>
2020-04-27 23:56:55 +0900

    사용자 이름 저장까지 #3.4

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const clock = document.querySelector(".clock");
-
 const getTime = () => {
+  const clock = document.querySelector(".clock");
   const now = new Date();
   const time = {
     h: now.getHours() >= 10 ? now.getHours() : `0${now.getHours()}`,
@@ -10,9 +9,30 @@ const getTime = () => {
   clock.innerHTML = `${time.h}:${time.m}:${time.s}`;
 };
 
+// ###
+const form = document.querySelector("form");
+const input = document.querySelector(".name");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  localStorage.setItem("currentUser", input.value);
+  getUser();
+};
+
+const getUser = () => {
+  const greeting = document.querySelector(".greeting");
+  const user = localStorage.getItem("currentUser");
+  if (user) {
+    input.classList.add("hidden");
+    greeting.innerHTML = `Hello ${user}`;
+    greeting.classList.remove("hidden");
+  }
+};
+
 const init = () => {
   getTime();
-
+  form.addEventListener("submit", handleSubmit);
+  getUser();
   setInterval(getTime, 1000);
 };
 
