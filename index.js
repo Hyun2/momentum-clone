d687f4d4eb18aff13fc94f024b1a004c8f30dc03
Hyun2 <hks0284@gmail.com>
2020-04-27 23:28:32 +0900

    clock 완성

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const h1 = document.querySelector("h1");
-h1.innerText = "Title is changed";
+const clock = document.querySelector(".clock");
+
+const getTime = () => {
+  const now = new Date();
+  const time = {
+    h: now.getHours() >= 10 ? now.getHours() : `0${now.getHours()}`,
+    m: now.getMinutes() >= 10 ? now.getMinutes() : `0${now.getMinutes()}`,
+    s: now.getSeconds() >= 10 ? now.getSeconds() : `0${now.getSeconds()}`,
+  };
+  clock.innerHTML = `${time.h}:${time.m}:${time.s}`;
+};
+
+const init = () => {
+  getTime();
+
+  setInterval(getTime, 1000);
+};
+
+init();
